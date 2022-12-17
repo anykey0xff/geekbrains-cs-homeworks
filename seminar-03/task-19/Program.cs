1dbd4a2b3da4a16d6cf6e4b5f5656036e60d00e4
Andrei Ivashchenko <anykey0xff@gmail.com>
2022-12-17 12:19:52 +0400

    upd task-19

--- seminar-03/task-19/Program.cs
+++ seminar-03/task-19/Program.cs
@@ -7,14 +7,18 @@ Console.Clear();
 Console.WriteLine("Программа принимает на вход пятизначное число и проверяет, является ли оно палиндромом.");
 
 Console.Write("Введите пятизначное число: ");
-string s = Console.ReadLine(); 
+string? s = Console.ReadLine();
 
-int[] numbers = new int[5];
-for (int i = 0; i < numbers.Length; i++) {
-    numbers[i] = Convert.ToInt32(Convert.ToString(s[i]));    
+if (s != null && s != "") {
+
+    int[] numbers = new int[5];
+    for (int i = 0; i < numbers.Length; i++) {
+            numbers[i] = Convert.ToInt32(Convert.ToString(s[i]));    
+    }
+
+    if (numbers[0] == numbers[4] && numbers[1] == numbers[3]) 
+        Console.WriteLine("да");
+    else
+        Console.WriteLine("нет");
+
 }
-
-if (numbers[0] == numbers[4] && numbers[1] == numbers[3]) 
-    Console.WriteLine("да");
-else
-    Console.WriteLine("нет");
